66a34ad6976852443f9cf11e9c5d66f74cedfb66
H A J U M <hajumh12@gmail.com>
2022-01-17 10:57:35 +0300

    Additional of toolbar

--- app/src/main/java/com/example/remainderapp/MainActivity.java
+++ app/src/main/java/com/example/remainderapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.remainderapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add:
+                Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.close:
+                Toast.makeText(this, "Close", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/remainderapp/menu_remainders.java
+++ app/src/main/java/com/example/remainderapp/menu_remainders.java
@@ -9,6 +9,6 @@ public class menu_remainders extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_remainders);
+
     }
 }
